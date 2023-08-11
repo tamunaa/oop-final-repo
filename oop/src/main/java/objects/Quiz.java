72fd03c8d97051fcd0b7ac06e6434c84650b0e6b
Tsiqara <mtsik21@freeuni.edu.ge>
2023-08-12 00:37:33 +0400

    Implement Quiz and Review classes

---
+++ oop/src/main/java/objects/Quiz.java
@@ -0,0 +1,98 @@
+package objects;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class Quiz {
+    private int Author;
+    private String quizName;
+    private String description;
+    private int timer;
+    private Timestamp dateCreated;
+    private boolean isRandom;
+    private boolean onOnePage;
+    private boolean correctImmediately;
+    private boolean isPractice;
+    private int ID = -1;
+
+    public Quiz(int author, String quizName, String description, int timer, Timestamp created,
+                boolean isRandom, boolean onOnePage,boolean correctImmediately, boolean isPractice){
+        Author = author;
+        this.quizName = quizName;
+        this.description = description;
+        this.timer = timer;
+        this.dateCreated = created;
+        this.isRandom = isRandom;
+        this.onOnePage = onOnePage;
+        this.correctImmediately = correctImmediately;
+        this.isPractice = isPractice;
+    }
+
+    public Quiz(int author, String quizName, String description, int timer){
+        this(author, quizName, description, timer, new Timestamp(new java.util.Date().getTime()), false, false, false, false);
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public void setRandom(boolean random) {
+        isRandom = random;
+    }
+
+    public void setOnOnePage(boolean onOnePage) {
+        this.onOnePage = onOnePage;
+    }
+
+    public void setCorrectImmediately(boolean correctImmediately) {
+        this.correctImmediately = correctImmediately;
+    }
+
+    public void setPractice(boolean practice) {
+        isPractice = practice;
+    }
+
+    public int getAuthor() {
+        return Author;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTimer() {
+        return timer;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public boolean isRandom() {
+        return isRandom;
+    }
+
+    public boolean isOnOnePage() {
+        return onOnePage;
+    }
+
+    public boolean correctImmediately() {
+        return correctImmediately;
+    }
+
+    public boolean isPractice() {
+        return isPractice;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
