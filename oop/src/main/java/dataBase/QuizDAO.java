2d2a4d89e996f0866027b1debdfec3f72d29d0c6
Tsiqara <mtsik21@freeuni.edu.ge>
2023-08-12 00:38:08 +0400

    Implement QuizDAO interface and DbQuizDAO class

---
+++ oop/src/main/java/dataBase/QuizDAO.java
@@ -0,0 +1,22 @@
+package dataBase;
+
+import objects.Quiz;
+import objects.Review;
+import objects.questions.Question;
+
+import java.util.List;
+
+public interface QuizDAO {
+    public int addQuiz(Quiz quiz);
+    public boolean removeQuiz(int id);
+    public List<Quiz> getQuizzesByAuthor(int author);
+    public List<Quiz> getQuizByQuizName(String name);
+    public Quiz getQuizByID(int id);
+    public List<Quiz> getPopularQuizzes(int top);
+    public List<Question> getQuestions(Quiz quiz);
+    public List<Quiz> recentlyCreatedQuizzes(int num);
+    public List<String> getTags(Quiz quiz);
+    public List<Review> getReviews(Quiz quiz);
+    public Double getRating(Quiz quiz);
+    public List<Quiz> getTopRatedQuizzes(int top);
+}
