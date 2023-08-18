d521dc03eb75d67589167fda642504e4eabe960a
tamar tsotniashvili <ttsot21@freeuni.edu.ge>
2023-08-19 01:06:37 +0400

    little changes, serialize, shuffle

--- oop/src/main/java/objects/questions/UnorderedMultiAnswer.java
+++ oop/src/main/java/objects/questions/UnorderedMultiAnswer.java
@@ -1,15 +1,14 @@
 package objects.questions;
 
-import java.awt.*;
 import java.io.Serializable;
 import java.util.HashSet;
 
 public class UnorderedMultiAnswer implements MultiAnswerType, Serializable {
     private final HashSet<String> correctAnswers;
     public UnorderedMultiAnswer(String[] answers) {
-        this.correctAnswers = new HashSet();
-        for (int i = 0; i < answers.length; i++) {
-            this.correctAnswers.add(answers[i].trim().toLowerCase());
+        this.correctAnswers = new HashSet<>();
+        for (String answer : answers) {
+            this.correctAnswers.add(answer.trim().toLowerCase());
         }
     }
     @Override
