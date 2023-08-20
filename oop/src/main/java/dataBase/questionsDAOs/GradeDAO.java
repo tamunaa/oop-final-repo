00d48aee1d35d0aca1591d86f3514cc12a68d967
NiaGogokhia <nigogo20@freeuni.edu.ge>
2023-08-20 14:05:55 +0400

    when author scores the quizz this dao saves score in db

---
+++ oop/src/main/java/dataBase/questionsDAOs/GradeDAO.java
@@ -0,0 +1,27 @@
+package dataBase.questionsDAOs;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class GradeDAO {
+    private Connection connection;
+
+    public void GradeyDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean updateScore(int historyId, int score) {
+        String query = "UPDATE HISTORY SET score = ? WHERE ID = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, score);
+            statement.setInt(2, historyId);
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
