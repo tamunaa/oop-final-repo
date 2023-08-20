84cef024e4cc8e6500d474fc7651cd173f614f6c
NiaGogokhia <nigogo20@freeuni.edu.ge>
2023-08-20 17:34:40 +0400

    response class, dao and tests

--- oop/src/main/java/dataBase/questionsDAOs/ResponseDAO.java
+++ oop/src/main/java/dataBase/questionsDAOs/ResponseDAO.java
@@ -1,5 +1,7 @@
 package dataBase.questionsDAOs;
 
+import objects.Response;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -27,7 +29,50 @@ public class ResponseDAO {
         }
     }
 
-    //for testing
+    //get responses which are not graded yet
+    public Response getResponseByHistory(int historyId) {
+        String query = "SELECT * FROM RESPONSES WHERE History_ID = ? AND Is_graded = ? LIMIT 1";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, historyId);
+            statement.setBoolean(2, false);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int id = resultSet.getInt("ID");
+                    int questionId = resultSet.getInt("Question_ID");
+                    int grade = resultSet.getInt("grade");
+                    boolean isGraded = resultSet.getBoolean("Is_graded");
+                    String responseText = resultSet.getString("Response");
+
+                    return new Response(id, questionId, historyId, grade, isGraded, responseText);
+                } else {
+                    return null; // No ungraded response found with the given IDs
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean addScoreAndMarkAsGraded(int responseId, int grade) {
+        String query = "UPDATE RESPONSES SET grade = ?, Is_graded = ? WHERE ID = ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, grade);
+            statement.setBoolean(2, true);
+            statement.setInt(3, responseId);
+
+            int rowsAffected = statement.executeUpdate();
+            return rowsAffected > 0; // Return true if at least one row was affected
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; // Return false in case of an error
+        }
+    }
+
+
+
+    //for tests
     public String getResponseByQuestionAndHistory(int questionId, int historyId) {
         String query = "SELECT * FROM RESPONSES WHERE Question_ID = ? AND History_ID = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
@@ -36,9 +81,6 @@ public class ResponseDAO {
 
             try (ResultSet resultSet = statement.executeQuery()) {
                 if (resultSet.next()) {
-//                    int responseId = resultSet.getInt("ID");
-//                    int grade = resultSet.getInt("grade");
-//                    boolean isGraded = resultSet.getBoolean("Is_graded");
                     String response = resultSet.getString("Response");
 
                     return response;
@@ -51,4 +93,6 @@ public class ResponseDAO {
             return null; // Return null in case of an error
         }
     }
+
+
 }
