9da1947dc1e86353733888ebbe84620fcdf2057c
TamarZerekidze <tzere21@freeuni.edu.ge>
2023-08-22 11:42:13 +0400

    few servlets for user profile front

---
+++ oop/src/main/java/servlets/SearchServlet.java
@@ -0,0 +1,40 @@
+package servlets;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+import dataBase.*;
+import objects.User;
+import objects.Quiz;
+
+
+@WebServlet(name = "SearchServlet", value = "/SearchServlet")
+public class SearchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        QuizDAO quizDAO = (DbQuizDAO) request.getServletContext().getAttribute("quizDAO");
+        UserDAO userDAO = (UserDAO) request.getServletContext().getAttribute("userDAO");
+        String searchInput = request.getParameter("searchInput");
+        String type = request.getParameter("type");
+        if (type.equals("quizName")) {
+            List<Quiz> quizzes = quizDAO.getQuizByQuizName(searchInput);
+            request.setAttribute("searchResult", quizzes);
+            request.getRequestDispatcher("/quizSearch.jsp").forward(request, response);
+
+        } else if (type.equals("quizTag")){
+            List<Quiz> quizzes = quizDAO.getQuizzesByTag(searchInput);
+            request.setAttribute("searchResult", quizzes);
+            request.getRequestDispatcher("/quizSearch.jsp").forward(request, response);
+        } else {
+                User user = userDAO.getUserByUsername(searchInput);
+                request.setAttribute("searchedUser", user);
+                request.getRequestDispatcher("/userSearch.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
