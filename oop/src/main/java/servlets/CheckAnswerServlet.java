656626071dadc0799e9155754ddec257e9289577
Tsiqara <mtsik21@freeuni.edu.ge>
2023-08-23 00:17:11 +0400

    add sendRedirect

--- oop/src/main/java/servlets/CheckAnswerServlet.java
+++ oop/src/main/java/servlets/CheckAnswerServlet.java
@@ -10,12 +10,13 @@ import objects.questions.Question;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
 public class CheckAnswerServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         QuizDAO quizDAO = (QuizDAO) request.getServletContext().getAttribute("quizDAO");
         QuestionsDAO questionsDAO = (QuestionsDAO) request.getServletContext().getAttribute("questionsDAO");
         Quiz currQuiz = (Quiz) request.getSession().getAttribute("currQuiz");
@@ -30,7 +31,8 @@ public class CheckAnswerServlet extends HttpServlet {
             int max_score = question.getCorrectAnswers().length;
             request.setAttribute("score", score + " / " + max_score);
             request.setAttribute("correctAnswer", question.getCorrectAnswers());
-            response.sendRedirect(რა კითხვის ფეიჯის იყო იმას რო გამოაჩინოს ქულა);
+//            response.sendRedirect(რა კითხვის ფეიჯის იყო იმას რო გამოაჩინოს ქულა);
+            response.sendRedirect(question.getQuestionType() + ".jsp");
         }
     }
 }
