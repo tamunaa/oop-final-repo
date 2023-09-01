bf4bc0596ad4ab1ace84d7260771068b8481f287
tamar tsotniashvili <ttsot21@freeuni.edu.ge>
2023-09-01 09:12:57 +0400

    quiz writting

--- oop/src/main/java/servlets/quizSummary.java
+++ oop/src/main/java/servlets/quizSummary.java
@@ -1,11 +1,8 @@
 package servlets;
 
-import dataBase.AchievementDAO;
 import dataBase.DbQuizDAO;
-import dataBase.HistoryDAO;
 import dataBase.QuizDAO;
 import objects.Quiz;
-import objects.User;
 import objects.questions.*;
 import org.apache.commons.dbcp2.BasicDataSource;
 import java.util.List;
@@ -27,21 +24,11 @@ public class quizSummary extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HistoryDAO historyDAO = (HistoryDAO) request.getServletContext().getAttribute("historyDAO");
-        AchievementDAO achievementDAO = (AchievementDAO) request.getServletContext().getAttribute("achievementDAO");
-        int userId = ((User)request.getSession().getAttribute("currUser")).getId();
-        if(historyDAO.getHistoryByUserId(userId).size() >= 10){
-            achievementDAO.addUserAchievement(userId, 6);
-        }
         request.getSession().setAttribute("endTime", new Timestamp(new java.util.Date().getTime()));
-        QuizDAO quizDAO = (QuizDAO) request.getServletContext().getAttribute("quizDAO");
-        if(quizDAO.getQuizByID(Integer.parseInt(request.getParameter("quizId"))).isPractice()){
-            achievementDAO.addUserAchievement(userId, 3);
-        }
-        if(historyDAO.sortedHistory(Integer.parseInt(request.getParameter("quizId")),"score", 1).get(0).getUserId() == userId){
-            achievementDAO.addUserAchievement(userId, 2);
-        }
+        System.out.println("return value " + request.getParameter("quizId"));
         request.getRequestDispatcher("quizResult.jsp?quizId=" + request.getParameter("quizId")).forward(request, response);
     }
+
+
 
 }
