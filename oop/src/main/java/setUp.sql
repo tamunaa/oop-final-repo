c6b55a192ea5a92dd88126c768c9d4e753148d70
tamar tsotniashvili <ttsot21@freeuni.edu.ge>
2023-09-20 01:19:07 +0400

    deleted comments

--- oop/src/main/java/setUp.sql
+++ oop/src/main/java/setUp.sql
@@ -1,3 +1,5 @@
+DELETE FROM ACHIEVEMENTS where 1=1;
+
 INSERT INTO ACHIEVEMENTS (Achievement_type, Picture_url, Achievement_desc)
 VALUES
     ('amateur', 'photos/amateur.jpeg', 'The user created a quiz'),
@@ -7,4 +9,13 @@ VALUES
     ('Prodigious Author', 'photos/Prodigious Author.jpg', 'The user created ten quizzes.'),
     ('Profilic Author', 'photos/Profilic Author.jpg', 'The user created five quizzes.'),
     ('Quiz Machine', 'photos/Quiz Machine.jpg', 'The user took ten quizzes.');
+;
+
+DELETE FROM USERS where 1=1;
+
+INSERT INTO USERS (Username, Email, Password_hash, Is_administrator)
+VALUES
+    ('admin', 'admin@example.com', '$2a$10$HXYIgiSCyzVADd6GEqv.9OOKLciRfghOKTVNFtQHgbOEOezDDYIY2', 1),
+    ('test1', 'test1@example.com', '$2a$10$HXYIgiSCyzVADd6GEqv.9OOKLciRfghOKTVNFtQHgbOEOezDDYIY2', 0),
+    ('test2', 'test2@example.com', '$2a$10$HXYIgiSCyzVADd6GEqv.9OOKLciRfghOKTVNFtQHgbOEOezDDYIY2', 0)
 ;
